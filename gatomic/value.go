9b54259f1d36e19d1d6dc471b6947472b434b0b5
Jacob Ochoa <jacob.ochoa@gmail.com>
2025-11-10 23:40:43 -0800

    Add gatomic Value implementation with tests
    - Implement generic atomic Value wrapper
    - Add comprehensive tests for Load, Store, Swap, and CompareAndSwap
    - Value always initializes to zero value for type safety

--- gatomic/value.go
+++ gatomic/value.go
@@ -1,30 +1,37 @@
 package gatomic
 
-import "sync/atomic"
+import (
+	"sync/atomic"
+)
 
 // Value wraps [sync/atomic.Value] with generics to make it a bit more
-// convenient to use in modern Go.
+// convenient to use in modern Go. It inits the value to the Zero value rather
+// then deal with existance of nil.
 type Value[T any] struct {
 	V atomic.Value
 }
 
+func (v *Value[T]) init() {
+	var x T
+	_ = v.V.CompareAndSwap(nil, x)
+}
+
 func (v *Value[T]) CompareAndSwap(old T, new T) (swapped bool) {
+	v.init()
 	return v.V.CompareAndSwap(old, new)
 }
 
-// Load returns the zero value of T or the stored value.
 func (v *Value[T]) Load() (val T) {
-	loaded := v.V.Load()
-	if loaded == nil {
-		return val
-	}
-	return loaded.(T)
+	v.init()
+	return v.V.Load().(T)
 }
 
 func (v *Value[T]) Store(val T) {
+	v.init()
 	v.V.Store(val)
 }
 
 func (v *Value[T]) Swap(new T) (old T) {
+	v.init()
 	return v.V.Swap(new).(T)
 }
